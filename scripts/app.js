c63fe436232b94804d867f95c6fe03c899767e38
sdcaulley <stephanie.caulley@gmail.com>
2016-12-13 17:10:04 -0800

    met standard goals

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+var project = [];
+
+var Project = function(opts) {
+    this.title = opts.title;
+    this.creation = opts.creation;
+    this.image = opts.image;
+    this.alt = opts.alt;
+    this.description = opts.description;
+    this.href = opts.href;
+};
+
+Project.prototype.toHtml = function() {
+    var $newArticle = $('article.template').clone();
+
+    $newArticle.find('time[pubdate]').attr('title', this.creation);
+    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.creation))/60/60/24/1000) + ' days ago');
+    $newArticle.find('a').attr('href', this.href).html(this.title);
+    $newArticle.find('.article-body').html(this.description);
+    $newArticle.find('img').attr('src', this.image).attr('alt', this.alt);
+
+    $newArticle.removeAttr('class');
+    return $newArticle;
+};
+
+projectsArray.forEach(function(ele) {
+    project.push(new Project(ele));
+});
---
+++ scripts/data.js
@@ -0,0 +1,20 @@
+var projectsArray = [
+    {title: 'Guessing Game',
+    creation: '2016-07-25',
+    image: 'img/guessing-game.png',
+    alt: 'screen shot of Guessing Game application',
+    description: 'My first coding project.  There are a series of questions asked and a score generated.  It seemed huge at the time.',
+    href: 'https://sdcaulley.github.io/guessing-game/'},
+    {title: 'Cookie Stand',
+    creation: '2016-08-09',
+    image: 'img/cookie-stand.png',
+    alt: 'screen shot of Cookie Store application',
+    description: 'My second coding project.  This was an exercise introducing objects, and object constructors.',
+    href: 'https://sdcaulley.github.io/cookie-stand/'},
+    {title: 'Bus Mall',
+    creation: '2016-09-05',
+    image: 'img/bus-mall.png',
+    alt: 'screen shot of Bus Mall application',
+    description: 'My third coding project.  This added a library - chart.js - and local storage.',
+    href: 'https://sdcaulley.github.io/market-analysis-app/'}
+];
