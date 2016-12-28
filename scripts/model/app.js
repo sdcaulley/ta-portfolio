671b77dae386f4958f6f8b65d8c90f83c82aebd7
sdcaulley <stephanie.caulley@gmail.com>
2016-12-28 14:36:55 -0800

    handlebars added

--- scripts/model/app.js
+++ scripts/model/app.js
@@ -11,16 +11,12 @@ var Project = function(opts) {
 };
 
 Project.prototype.toHtml = function() {
-    var $newArticle = $('article.template').clone();
-    $newArticle.attr('data-category', this.category);
-    $newArticle.find('time[pubdate]').attr('title', this.creation);
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.creation))/60/60/24/1000) + ' days ago');
-    $newArticle.find('h1 a').attr('href', this.href).html(this.title);
-    $newArticle.find('.article-body').html(this.description);
-    $newArticle.find('img').attr('src', this.image).attr('alt', this.alt);
-
-    $newArticle.removeAttr('class');
-    return $newArticle;
+    this.daysAgo = parseInt((new Date() - new Date(this.creation))/60/60/24/1000);
+    this.publishedOn = 'about ' + this.daysAgo + 'days ago';
+    var source = $('#template').html();
+    var template = Handlebars.compile(source);
+    var html = template(this);
+    return html;
 };
 
 projectsArray.sort(function(curElem, nextElem) {
